53a86fd53b7a98be9296e981a5c85843d264d342
marvin <marvin@frobnitz.co.uk>
2019-08-17 15:05:57 +0100

    Rewrote iff.py almost entirely

--- iff.py
+++ iff.py
@@ -1,120 +1,110 @@
 # Copyright (C) 2001 - 2019 David Fillmore
-#
-# This file is part of buffle.
-#
-# buffle is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# buffle is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
 
 
-from string import *
+def get_chunk(data, position):
+    chunk_length = int.from_bytes(data[position+4:position+8], byteorder='big')
+    c = data[position:position+8+chunk_length]
+    return c
+
+def split_chunks(data):
+    """find all the top-level chunks in a bytes object, and return a list with each chunk as an item of bytes"""
+    pos = 0
+    chunks = []
+    while pos < len(data):
+        c = get_chunk(data, pos)
+        pos += chunk_length + 8
+        if len(c) % 2 == 1:
+            pos += 1
+        chunks.append(c)
+    return chunks
 
 class chunk:
-    ID = '    '
-    subID = '    '
+    ID = "    "
     length = 0
-    data = []
-    def dowrite(self, input=0):
-        if input != 0:
-            self.input = input
-        self.write()
-        self.length = len(self.data)
-        if self.length % 2 == 1:
-            self.data.append(0)
-               
-    def writeID(self):
-        return list(self.ID)
-    
-    def writelen(self):
-        clen = []
-        clen.append((self.length >> 24) & 0xff)
-        clen.append((self.length >> 16) & 0xff)
-        clen.append((self.length >> 8) & 0xff)
-        clen.append(self.length & 0xff)
-        return clen
+    data = b''
+    full_data = b'    \x00\x00\x00\x00'
+    def from_bytes(self, raw_data):
+        """given a bytes object containing an IFF chunk, return a chunk object of that data"""
+        self.full_data = raw_data
+        self.process_data()
+        self.update_length()
+        return self
 
-    def readID(self):
-        id = []
-        for a in range(4):
-            id.append(chr(self.data[a]))
-        id2 = ''.join(id)
-        return id2
+    def to_bytes(self):
+        """return a bytes object containing the chunk data"""
+        # update data
+        self.create_data()
+        self.update_length()
+        return self.full_data
 
-    def readlen(self):
-        string = self.data
-        len = (((((string[4] << 8) + string[5]) << 8) + string[6]) << 8) + string[7]
-        return len
+    def process_data(self):
+        """updates the various chunk attributes using the full_data"""
+        self.ID = self.full_data[0:4].decode('ascii')
+        self.length = int.from_bytes(self.full_data[4:8], byteorder='big')
+        self.data = self.full_data[8:]
 
-    def fbnum(self, string):
-        num = (((((ord(string[0]) << 8) + ord(string[1])) << 8) + ord(string[2])) << 8) + ord(string[3])
-        return num
+    def create_data(self):
+        """updates the full_data using the chunk attributes"""
+        self.full_data = self.ID.encode() + self.length.to_bytes(4, 'big') + self.data
+
+    def update_length(self):
+        self.length = len(self.full_data) - 8
+        self.full_data = self.full_data[0:4] + self.length.to_bytes(4, 'big') + self.full_data[8:]
+        
+class form_chunk(chunk):
+    ID = 'FORM'
+    sub_chunks = []
+    subID = '    '
+
+    def process_data(self):
+        """updates the various chunk attributes using the full_data"""
+        #should check to see if ID is FORM and complain otherwise
+        self.length = int.from_bytes(self.full_data[4:8], byteorder='big')
+        self.subID = self.full_data[8:12].decode('ascii')
+        self.data = self.full_data[12:]
+        sub_chunks_data = split_chunks(self.data)
+        self.sub_chunks = []
+        # should use self.data to find all sub chunks, then process them
+        for cd in sub_chunks_data:
+            ID = cd[0:4].decode('ascii')
+            if ID in chunk_types:
+                co = chunk_types[ID]().from_bytes(cd)
+            else:
+                co = chunk().from_bytes(cd)
+            self.sub_chunks.append(co)
+
+    def create_data(self):
+        chunks_data = []
+        
+        self.full_data = self.ID.encode() + self.length.to_bytes(4, 'big') + self.subID.decode()
+        
+        for co in self.sub_chunks():
+            cd = co.to_bytes()
+            self.full_data += cd
                 
-    def doread(self, data):
-        len = self.readlen(data)
-        self.data = []
-        for a in range(len):
-            self.data.append(data[a])
-        self.read()
+        
+class text_chunk(chunk): # any chunk where the data is pure text
+    def process_data(self):
+        """updates the various chunk attributes using the full_data""" 
+        self.length = int.from_bytes(self.full_data[4:8], byteorder='big')
+        self.text = self.full_data[8:].decode('latin-1')
 
-    def read(self):
-        pass
-
-    def write(self):
-        pass
-                        
-class authchunk(chunk):
+    def create_data(self):
+        """updates the full_data using the chunk attributes"""
+        self.full_data = self.ID.encode() + self.length.to_bytes(4, 'big') + self.text.encode()
+    
+class auth_chunk(text_chunk):
     ID = 'AUTH'
 
-class annochunk(chunk):
+
+class anno_chunk(text_chunk):
     ID = 'ANNO'
 
-class copychunk(chunk):
+class copy_chunk(text_chunk):
     ID = '(c) '
 
-class formchunk(chunk):
-    ID = 'FORM'
-    data = []
-    wchunks = []
-    def write(self):
-        self.data.append(ord(self.subID[0])) 
-        self.data.append(ord(self.subID[1]))
-        self.data.append(ord(self.subID[2]))
-        self.data.append(ord(self.subID[3]))
-        chunks = self.wchunks # chunks to write
-        for a in chunks:
-            cchunk = a() # set cchunk to current chunk
-            cchunk.dowrite(self.input) # write current chunk's data
-            id = cchunk.writeID() # set id to current chunk's ID
-            for b in range(len(id)):
-                self.data.append(ord(id[b])) # write current chunk's ID to data
-            clen = cchunk.writelen() # write current
-                        
-            for b in clen:
-                self.data.append(b)
-            for b in cchunk.data:
-                self.data.append(b)
-
-    def read(self):
-        place = 0
-        datachunk = self.data[8:len(self.data)]
-        while place < len(self.data):
-            clen = self.readlen(datachunk)
-            id = self.readID(datachunk)
-            datachunk = datachunk[place+8:clen]
-            try:
-                cchunk = self.chunks[id]()
-            except:
-                cchunk = chunk()
-            cchunk.read(datachunk)
-            place += clen
-            if clen % 2 == 1:
-                place += 1
-
-
-                 
+chunk_types = {'FORM':form_chunk,
+               'AUTH':auth_chunk,
+               'ANNO':anno_chunk,
+               '(c) ':copy_chunk
+              }
