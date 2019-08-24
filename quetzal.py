a7295f33add280430169d7b61c0f9f88f9fa8be0
marvin <marvin@frobnitz.co.uk>
2019-08-24 23:33:43 +0100

    Updated quetzal.py to new iff.py code

--- quetzal.py
+++ quetzal.py
@@ -1,18 +1,14 @@
 # Copyright (C) 2001 - 2019 David Fillmore
 
-
 import copy
 import os
 
 import iff
-
-
-
-
+from ifchunks import ifhd_chunk
             
-class umemchunk(iff.chunk):
+class umem_chunk(iff.chunk):
     ID = 'UMem'
-    data = []
+
     def write(self):
         self.data = storydata.memory[:]
 
@@ -21,12 +17,34 @@ class umemchunk(iff.chunk):
         storydata.memory = self.data[:]
         return self.data[:]
 
+    def process_data(self):
+        """updates the various chunk attributes using the raw_data"""
+        self.ID = self.raw_data[0:4].decode('ascii')
+        self.length = int.from_bytes(self.raw_data[4:8], byteorder='big')
+        self.dynamic_memory = self.raw_data[8:self.length+8]
 
-class cmemchunk(iff.chunk):
+    def create_data(self):
+        """updates the raw_data using the chunk attributes"""
+        length = len(self.data)
+        self.raw_data = self.ID.encode() + length.to_bytes(4, 'big') + self.dynamic_memory
+
+
+
+class cmem_chunk(iff.chunk):
     ID = 'CMem'
-    data = []
+
+    def process_data(self):
+        """updates the various chunk attributes using the raw_data"""
+        self.ID = self.raw_data[0:4].decode('ascii')
+        self.length = int.from_bytes(self.raw_data[4:8], byteorder='big')
+        self.dynamic_memory = self.raw_data[8:self.length+8]
+
+    def create_data(self):
+        """updates the raw_data using the chunk attributes"""
+        length = len(self.data)
+        self.raw_data = self.ID.encode() + length.to_bytes(4, 'big') + self.dynamic_memory
+
     def write(self):
-        #mem = []
         commem = []
         mem = [storydata.memory[a] ^ storydata.omemory[a] for a in range(len(storydata.memory))]
 
@@ -183,6 +201,7 @@ class ifhdchunk(iff.chunk):
     PC = 0
     data = []
     def write(self):
+        
         self.data.append(storydata.release >> 8) # release number
         self.data.append(storydata.release & 255)
 
@@ -215,12 +234,9 @@ class intdchunk(iff.chunk):
     reserved = 0
     terpID = '    '
     xdata = 0
-    data = []
 
-class formchunk(iff.chunk):
-    ID = 'FORM'
+class formchunk(iff.form_chunk):
     subID = 'IFZS'
-    data = []
 
     release = 0
     serial = '      '
@@ -284,6 +300,200 @@ class formchunk(iff.chunk):
             
             if cchunk.read() == -1:
                 return -1
+
+###
+
+class memory_chunk(iff.chunk):
+    def process_data(self):
+        """updates the various chunk attributes using the raw_data"""
+        self.ID = self.raw_data[0:4].decode('ascii')
+        self.length = int.from_bytes(self.raw_data[4:8], byteorder='big')
+        self.dynamic_memory = self.raw_data[8:self.length+8]
+
+    def create_data(self):
+        """updates the raw_data using the chunk attributes"""
+        length = len(self.dynamic_memory)
+        self.raw_data = self.ID.encode() + length.to_bytes(4, 'big') + self.dynamic_memory
+
+    
+class cmem_chunk(memory_chunk):
+    ID = 'CMem'
+
+
+class umem_chunk(memory_chunk):
+    ID = 'UMem'
+
+class frame:
+
+    retPC = 0
+    discard_result = False
+    varnum = 0
+    numargs = 0
+    lvars = []
+    evalstack = []
+
+    def __init__(self, retPC, discard_result, varnum, numargs, lvars, evalstack):
+        self.retPC = retPC
+        self.discard_result = discard_result
+        self.varnum = varnum
+        self.numargs = numargs
+        self.lvars = lvars[:]
+        self.evalstack = evalstack[:]
+        
+class stks_chunk(iff.chunk):
+    ID = 'Stks'
+    self.callstack = []
+
+    def process_data(self):
+        self.ID = self.raw_data[0:4].decode('ascii')
+        self.length = int.from_bytes(self.raw_data[4:8], byteorder='big')
+        self.callstack = []
+
+        p = 8
+
+        place = 0
+        while place < len(self.raw_data):
+            lvars = []
+            evalstack = []
+            
+            retPC = int.from_bytes(self.raw_data[p:p+3], byteorder='big')
+
+            numvars = self.raw_data[p+3] & 15
+            if self.raw_data[p+3] & 16:
+                discard_result = True
+            else:
+                discard_result = False
+
+            varnum = self.raw_data[p+4]
+
+            numargs = 0
+            args = self.raw_data[p+5]
+            while args > 0:
+                numargs += 1
+                args = args >> 1
+
+            evalstacksize = int.from_bytes(self.raw_data[p+6:p+7], byteorder='big')
+            
+            p += 8
+            for x in range(numvars): 
+                lvars.append(int.from_bytes(self.raw_data[p:p+1], byteorder='big'))
+                p += 2
+            
+            for x in range(evalstacksize):
+                evalstack.append(int.from_bytes(self.raw_data[p:p+1], byteorder='big'))
+                place += 2
+
+            self.callstack.append(frame(retPC, discard_result, varnum, numargs, lvars, evalstack))
+
+        
+
+    def write(self):
+        global zstack
+        data = bytearray()
+        data.extend(self.ID.encode())
+        data.extend(self.length.to_bytes(4, 'big'))
+        
+        for a in self.callstack:
+            data.extend(a.retPC.to_bytes(3, 'big'))
+            flags = len(a.lvars)
+            if a.discard_result:
+                flags += 16
+                    
+            data.extend(flags.to_bytes(1, 'big'))
+            data.extend(a.varnum.to_bytes(1, 'big'))
+            args = 1
+            for b in range(storydata.callstack[a].numargs):
+                args *= 2
+            args -= 1
+            self.data.append(args)
+            self.data.append((storydata.callstack[a].evalstacksize >> 8) & 0xff)
+            self.data.append(storydata.callstack[a].evalstacksize & 0xff)
+
+            for x in range(len(storydata.callstack[a].lvars)):
+                self.data.append((storydata.callstack[a].lvars[x] >> 8) & 255)
+                self.data.append(storydata.callstack[a].lvars[x] & 255)
+            
+            for x in range(storydata.callstack[a].evalstacksize):
+                self.data.append((storydata.callstack[a].evalstack[x] >> 8) & 255)
+                self.data.append(storydata.callstack[a].evalstack[x] & 255)
+        self.data.append((storydata.currentframe.retPC >> 16) & 0xff)
+        self.data.append((storydata.currentframe.retPC >> 8) & 0xff)
+        self.data.append(storydata.currentframe.retPC & 0xff)
+        self.data.append(storydata.currentframe.flags)
+        self.data.append(storydata.currentframe.varnum)
+        args = 1
+        for a in range(storydata.currentframe.numargs):
+            args *= 2
+        args -= 1
+        self.data.append(args)
+        self.data.append((len(storydata.currentframe.evalstack) >> 8) & 0xff)
+        self.data.append(len(storydata.currentframe.evalstack) & 0xff)
+        for x in range(len(storydata.currentframe.lvars)):
+            self.data.append((storydata.currentframe.lvars[x] >> 8) & 255)
+            self.data.append(storydata.currentframe.lvars[x] & 255)
+            
+        for x in range(storydata.currentframe.evalstacksize):
+            self.data.append((storydata.currentframe.evalstack[x] >> 8) & 255)
+            self.data.append(storydata.currentframe.evalstack[x] & 255)
+
+
+        
+        
+
+
+class intd_chunk(iff.chunk):
+    ID = 'IntD'
+    osID = '    '
+    machine_specific = False
+    do_not_copy = False
+    contID = 0
+    terpID = '    '
+    data = 0
+
+    def process_data(self):
+        self.ID = self.raw_data[0:4].decode('ascii')
+        self.length = int.from_bytes(self.raw_data[4:8], byteorder='big')
+        self.OSid = self.raw_data[8:12].decode('ascii')
+        flags = int.from_bytes(self.raw_data[12], byteorder='big')
+        if flags & 1:
+            self.do_not_copy = True
+        else:
+            self.do_not_copy = False
+        if flags & 2:
+            self.machine_specific = True
+        else:
+            self.machine_specific = False
+        self.contID = int.from_bytes(self.raw_data[13], byteorder='big')
+        self.OSid = self.raw_data[16:20].decode('ascii')
+        self.data = self.raw_data[20:]
+
+    def create_data(self):
+        data = bytearray()
+        data.extend(self.ID.encode())
+        data.extend(self.length.to_bytes(4, 'big'))
+        data.extend(self.osID.encode())
+        flags = 0
+        if self.do_not_copy:
+            flags += 1
+        if self.machine_specific:
+            flags += 2
+        data.extend(flags.to_bytes(1, 'big'))
+        data.extend(self.contID.to_bytes(1, 'big'))
+        data.extend(int(0).to_bytes(2, 'big'))
+        data.extend(self.terpID.encode())
+        data.extend(self.data)
+        data[4:8] = len(data).to_bytes(4, 'big')
+        self.raw_data = bytes(data)
+        
+        
+                    
+        
+
+class quetzal_chunk(iff.form_chunk):
+    subID = 'IFZS'
+    
+
+    
 
 
 class qdata:
