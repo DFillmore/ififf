246fced890f17fa7d36f3ee7c8bf797c43d3779a
DFillmore <marvin@frobnitz.co.uk>
2024-04-10 00:28:59 +0100

    Added copyright statements

--- quetzal.py
+++ quetzal.py
@@ -1,4 +1,16 @@
-# Copyright (C) 2001 - 2019 David Fillmore
+# Copyright (C) 2001 - 2024 David Fillmore
+#
+# This file is part of ififf.
+#
+# ififf is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# ififf is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
 import copy
 import os
